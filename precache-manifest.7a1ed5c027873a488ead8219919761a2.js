71a2b53cc276d1d7379bc10f40d971f73f0f0c4e
kodevx <rohithunni19@yahoo.com>
2019-08-28 12:36:10 +0530

    Updates

---
+++ precache-manifest.7a1ed5c027873a488ead8219919761a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26632a71f4c6c31ca85e93582c8096ab",
+    "url": "/cal-react/index.html"
+  },
+  {
+    "revision": "9bf0f6ea3864b9697cff",
+    "url": "/cal-react/static/css/main.07e4ffa5.chunk.css"
+  },
+  {
+    "revision": "891a21d37ab0818e427f",
+    "url": "/cal-react/static/js/2.46b72190.chunk.js"
+  },
+  {
+    "revision": "9bf0f6ea3864b9697cff",
+    "url": "/cal-react/static/js/main.d3da4b1e.chunk.js"
+  },
+  {
+    "revision": "c266ba509611d012181a",
+    "url": "/cal-react/static/js/runtime~main.a8fe408b.js"
+  },
+  {
+    "revision": "4b7eaff9f9368039185816d426927556",
+    "url": "/cal-react/static/media/panton-lightcaps-webfont.4b7eaff9.woff2"
+  },
+  {
+    "revision": "f0ed58627c4caeab407b724cd0990748",
+    "url": "/cal-react/static/media/panton-lightcaps-webfont.f0ed5862.woff"
+  }
+]);
